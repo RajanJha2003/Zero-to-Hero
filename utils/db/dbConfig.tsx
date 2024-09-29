bdeb3c287432a6029be2182bef6bb658106c788d
Rajan <rajanj448@student.sfit.ac.in>
2024-09-29 12:20:07 +0530

    db config

---
+++ utils/db/dbConfig.tsx
@@ -0,0 +1,11 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-http";
+import * as schema from "./schema";
+
+const sql = neon(
+  "postgresql://zero2hero_owner:xnIR6kA8UhOt@ep-noisy-star-a1k2ifif.ap-southeast-1.aws.neon.tech/zero2hero?sslmode=require"
+);
+
+
+
+export const db=drizzle(sql,{schema})
