fa523eafa301bdb2bb85b3ee46eec68ff51fa070
Rajan <rajanj448@student.sfit.ac.in>
2024-09-29 12:11:58 +0530

    collectedwaste schema

--- utils/db/actions.ts
+++ utils/db/actions.ts
@@ -1,84 +1,67 @@
-  import {integer, varchar, pgTable, serial, text, timestamp, jsonb, boolean } from 'drizzle-orm/pg-core';
+import {
+  integer,
+  varchar,
+  pgTable,
+  serial,
+  text,
+  timestamp,
+  jsonb,
+  boolean,
+} from "drizzle-orm/pg-core";
+
+export const Users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  email: varchar("email", { length: 255 }).notNull().unique(),
+  name: varchar("name", { length: 255 }).notNull(),
+  createdAt: timestamp("created-at").defaultNow().notNull(),
+});
+
+export const Reports = pgTable("reports", {
+  id: serial("id").primaryKey(),
+  userId: integer("user_id")
+    .references(() => Users.id)
+    .notNull(),
+  location: text("location").notNull(),
+  wasteType: varchar("waste_type", { length: 255 }).notNull(),
+  amount: varchar("amount", { length: 255 }).notNull(),
+  imageurl: text("image_url"),
+  verificationResult: jsonb("Verification_result"),
+  status: varchar("status", { length: 255 }).notNull().default("pending"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+  collectorId: integer("collector_id").references(() => Users.id),
+});
+
+export const Rewards = pgTable("rewards", {
+  id: serial("id").primaryKey(),
+  userid: integer("user_id")
+    .references(() => Users.id)
+    .notNull(),
+  points: integer("points").notNull().default(0),
+  level: integer("level").notNull().default(1),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").defaultNow().notNull(),
+  isAvailable: boolean("is_available").notNull().default(true),
+  description: text("description"),
+  name: varchar("name", { length: 255 }).notNull(),
+  collectionInfo: text("collection_info").notNull(),
+});
 
 
-
-export const Users=pgTable("users",{
+export const CollectedWastes=pgTable("collected_wastes",{
     id:serial("id").primaryKey(),
-    email:varchar("email",{length:255}).notNull().unique(),
-    name:varchar("name",{length:255}).notNull(),
-    createdAt:timestamp("created-at").defaultNow().notNull(),
-
+    reportId:integer("report_id").references(()=>Reports.id).notNull(),
+    collectorId:integer("collector_id").references(()=>Users.id).notNull(),
+    collectionData:timestamp("collection_data").notNull(),
+    status:varchar("status",{length:20}).notNull().default("collected"),
 })
 
-
-export const Reports=pgTable("reports",{
-    id:serial("id").primaryKey(),
-    userId:integer("user_id").references(()=>Users.id).notNull(),
-    location:text("location").notNull(),
-    wasteType:varchar("waste_type",{length:255}).notNull(),
-    amount:varchar("amount",{length:255}).notNull(),
-    imageurl:text("image_url"),
-    verificationResult:jsonb("Verification_result"),
-    status:varchar("status",{length:255}).notNull().default("pending"),
-    createdAt:timestamp("created_at").defaultNow().notNull(),
-    collectorId:integer("collector_id").references(()=>Users.id),
-
-
-})
-
-
-
-export const Rewards=pgTable("rewards",{
-    id:serial("id").primaryKey(),
-    userid:integer("user_id").references(()=>Users.id).notNull(),
-   points:integer("points").notNull().default(0),
-   level:integer("level").notNull().default(1),
-   createdAt:timestamp("created_at").notNull().defaultNow(),
-   updatedAt:timestamp("updated_at").defaultNow().notNull(),
-   isAvailable:boolean("is_available").notNull().default(true),
-   description:text("description"),
-   name:varchar("name",{length:255}).notNull(),
-   collectionInfo:text("collection_info").notNull()
-
-
-})
-
-export const Notifications=pgTable("notifications",{
-    id:serial("id").primaryKey(),
-    userId:integer("user_id").references(()=>Users.id).notNull(),
-    message:text("message").notNull(),
-    type:varchar("type",{length:50}).notNull(),
-    isRead:boolean("is_read").notNull().default(false),
-    createdAt:timestamp("created_at").defaultNow().notNull()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-})
+export const Notifications = pgTable("notifications", {
+  id: serial("id").primaryKey(),
+  userId: integer("user_id")
+    .references(() => Users.id)
+    .notNull(),
+  message: text("message").notNull(),
+  type: varchar("type", { length: 50 }).notNull(),
+  isRead: boolean("is_read").notNull().default(false),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+});
