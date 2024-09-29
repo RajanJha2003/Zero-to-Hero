e22b4ec83a92772baba2f219ca4aaf63f30fb05b
Rajan <rajanj448@student.sfit.ac.in>
2024-09-29 11:55:25 +0530

    Report schema

--- utils/db/actions.ts
+++ utils/db/actions.ts
@@ -9,3 +9,19 @@ export const Users=pgTable("users",{
     createdAt:timestamp("created-at").defaultNow().notNull(),
 
 })
+
+
+export const Reports=pgTable("reports",{
+    id:serial("id").primaryKey(),
+    userid:integer("user_id").references(()=>Users.id).notNull(),
+    location:text("location").notNull(),
+    wasteType:varchar("waste_type",{length:255}).notNull(),
+    amount:varchar("amount",{length:255}).notNull(),
+    imageurl:text("image_url"),
+    verificationResult:jsonb("Verification_result"),
+    status:varchar("status",{length:255}).notNull().default("pending"),
+    createdAt:timestamp("created_at").defaultNow().notNull(),
+    collectorId:integer("collector_id").references(()=>Users.id),
+
+
+})
