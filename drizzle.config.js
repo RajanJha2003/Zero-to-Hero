0ffb38c68bdac60d0d6158c8283324357205f3b3
Rajan <rajanj448@student.sfit.ac.in>
2024-09-29 12:25:35 +0530

    drizzle config

---
+++ drizzle.config.js
@@ -0,0 +1,11 @@
+export default {
+    dialect: "postgresql",
+    schema: "./src/utils/db/schema.ts",
+    out: "./drizzle",
+    dbCredentials: {
+      url: "postgresql://zero2hero_owner:xnIR6kA8UhOt@ep-noisy-star-a1k2ifif.ap-southeast-1.aws.neon.tech/zero2hero?sslmode=require",
+      connectionString:
+        "postgresql://zero2hero_owner:xnIR6kA8UhOt@ep-noisy-star-a1k2ifif.ap-southeast-1.aws.neon.tech/zero2hero?sslmode=require"
+    },
+  };
+  
--- utils/db/dbConfig.tsx
+++ utils/db/dbConfig.tsx
@@ -2,9 +2,7 @@ import { neon } from "@neondatabase/serverless";
 import { drizzle } from "drizzle-orm/neon-http";
 import * as schema from "./schema";
 
-const sql = neon(
-  "postgresql://zero2hero_owner:xnIR6kA8UhOt@ep-noisy-star-a1k2ifif.ap-southeast-1.aws.neon.tech/zero2hero?sslmode=require"
-);
+const sql = neon("postgresql://zero2hero_owner:xnIR6kA8UhOt@ep-noisy-star-a1k2ifif.ap-southeast-1.aws.neon.tech/zero2hero?sslmode=require");
 
 
 
